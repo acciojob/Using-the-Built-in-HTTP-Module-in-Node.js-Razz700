7ffcab6c5b0b6295d3fb7728cf7eee9716fbb611
Razzakh <mrazzakh123@gmail.com>
2024-09-06 19:11:19 +0530

    first

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const http = require('http');
-
+const fs=require('fs');
+const server=http.createServer((req,res)=>{
+if(fs.readFileSync("./output.txt")){
+fs.readFile('./output.txt','utf8',(err,data)=>{
+    console.log(data);
+    res.write('Hello, World!');
+})
+}else{
+    console.log('File cannot be read!')
+}
+});
+server.listen(3000,'localhost',()=>{
+    console.log('Listenig to port 3000');
+})
 // TODO: Create an HTTP server
 
 // TODO: Listen on port 3000
